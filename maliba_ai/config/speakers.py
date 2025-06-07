5cf5742ef8aed0f9963fca16cc35588c5959e3fb
sudoping01 <sudoping01@gmail.com>
2025-06-07 05:20:23 +0000

    speakers definitions

---
+++ maliba_ai/config/speakers.py
@@ -0,0 +1,9 @@
+from maliba_ai.config.settings import Speakers
+from maliba_ai.config.settings import SPEAKER_IDS
+from maliba_ai.config.settings import SingleSpeaker
+
+Adame   = Speakers.Adame
+Moussa  = Speakers.Moussa
+Bourama = Speakers.Bourama
+Modibo  = Speakers.Modibo
+Seydou  = Speakers.Seydou
