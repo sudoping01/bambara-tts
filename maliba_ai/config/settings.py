9f0b68804a50b9a5d8becaf066fe699f54b059fd
sudoping01 <sudoping01@gmail.com>
2025-06-14 19:19:58 +0000

    add reminding speaker

--- maliba_ai/config/settings.py
+++ maliba_ai/config/settings.py
@@ -5,7 +5,7 @@ from typing import List
 MODEL_REPO = "sudoping01/bambara-tts-1-merged-16bit"
 BASE_SPARK_MODEL= "unsloth/Spark-TTS-0.5B"
 
-SPEAKER_IDS = ["SPEAKER_1", "SPEAKER_2", "SPEAKER_3", "SPEAKER_4", "SPEAKER_5"]
+SPEAKER_IDS = ["SPEAKER_1", "SPEAKER_2", "SPEAKER_3", "SPEAKER_4", "SPEAKER_5", "SPEAKER_6", "SPEAKER_7", "SPEAKER_8", "SPEAKER_9", "SPEAKER_10"]
 
 
 @dataclass
@@ -24,15 +24,21 @@ class SingleSpeaker:
 
 
 class Speakers:
-    Adame: SingleSpeaker = SingleSpeaker(id="SPEAKER_1")
-    Moussa: SingleSpeaker = SingleSpeaker(id="SPEAKER_2")
-    Bourama: SingleSpeaker = SingleSpeaker(id="SPEAKER_3")
-    Modibo: SingleSpeaker = SingleSpeaker(id="SPEAKER_4")
-    Seydou: SingleSpeaker = SingleSpeaker(id="SPEAKER_5")
-    
+    Adama: SingleSpeaker     = SingleSpeaker(id="SPEAKER_1")
+    Moussa: SingleSpeaker    = SingleSpeaker(id="SPEAKER_2")
+    Bourama: SingleSpeaker   = SingleSpeaker(id="SPEAKER_3")
+    Modibo: SingleSpeaker    = SingleSpeaker(id="SPEAKER_4")
+    Seydou: SingleSpeaker    = SingleSpeaker(id="SPEAKER_5")
+    Amadou : SingleSpeaker   = SingleSpeaker(id="SPEAKER_6")
+    Bakary: SingleSpeaker    = SingleSpeaker(id="SPEAKER_7")
+    Ngolo: SingleSpeaker     = SingleSpeaker(id="SPEKAER_8")
+    Amara: SingleSpeaker     = SingleSpeaker(id="SPEAKER_9")
+    Ibrahima : SingleSpeaker = SingleSpeaker(id="SPEAKER_10")
+
+
     @classmethod
     def get_all_speakers(cls) -> List[SingleSpeaker]:
-        return [cls.Adame, cls.Moussa, cls.Bourama, cls.Modibo, cls.Seydou]
+        return [cls.Adama, cls.Moussa, cls.Bourama, cls.Modibo, cls.Seydou, cls.Amadou, cls.Bakary, cls.Ngolo, cls.Amadou, cls.Ibrahima]
     
     @classmethod
     def get_speaker_by_name(cls, name: str) -> SingleSpeaker:
